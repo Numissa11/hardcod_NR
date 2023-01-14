333b4207726539e416c87f7e65e8e160033c1fcd
Numissa11 <61435050+Numissa11@users.noreply.github.com>
2023-01-14 04:40:24 +0100

    http request working

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
+const PORT = 6868
+app.use(express.json())
+
+app.get('/api/my-app', (req, res) => {
+    res.send({
+        msg: 'Hello my friends ! '
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`server listen on port: ${PORT}` )
+})
