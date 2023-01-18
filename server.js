d0595ab63fdd7fc8e38fd5b199db7b25109cfe27
Numissa11 <61435050+Numissa11@users.noreply.github.com>
2023-01-18 21:45:59 +0100

    response html react build

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const path = require('path')
 require('dotenv').config()
 const app = express()
+
 const PORT = 6868
+
 app.use(express.json())
+
+app.use(express.static('front/build'))
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, './front/build/index.html'))
+})
 
 app.get('/api/my-app', (req, res) => {
     res.send({
